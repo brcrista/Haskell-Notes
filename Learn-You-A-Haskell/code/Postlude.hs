c895a6714b91e3615048c69205d140e927d36105
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-01-09 18:04:37 -0500

    Forward pipe operator

--- Learn-You-A-Haskell/code/Postlude.hs
+++ Learn-You-A-Haskell/code/Postlude.hs
@@ -1,15 +1,12 @@
 -- This module contains Prelude-like functions that aren't actually in Prelude.
 module Postlude(
-  apply,
-  compose,
+  (|>),
   negative
 )
 where
 
-apply :: (a -> b) -> a -> b
-apply f = f
+(|>) :: a -> (a -> b) -> b
+(|>) = flip ($)
 
-compose :: (b -> c) -> (a -> b) -> (a -> c)
-compose f g x = f $ g x
-
-negative = compose negate abs
+negative :: Integer -> Integer
+negative = negate . abs
