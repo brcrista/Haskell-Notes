322be27b1fbfa6b3302b9af883587958eb523265
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-01-09 14:22:20 -0500

    Get RPN working with Fractional

--- Learn-You-A-Haskell/code/rpn.hs
+++ Learn-You-A-Haskell/code/rpn.hs
@@ -8,27 +8,27 @@ main = do
 
 data Operator = Add | Subtract | Multiply | Divide
 
-evaluateRPN :: String -> Int
+evaluateRPN :: (Fractional a, Read a) => String -> a
 evaluateRPN rpn = evaluateRPNRecursive parsedRPN []
   where
     parsedRPN = map parse (words rpn)
 
-evaluateRPNRecursive :: [Either Int Operator] -> [Int] -> Int
+evaluateRPNRecursive :: Fractional a => [Either a Operator] -> [a] -> a
 evaluateRPNRecursive [] [x] = x
 evaluateRPNRecursive [] _ = error "invalid expression"
 evaluateRPNRecursive ((Left num) : rest) xs = evaluateRPNRecursive rest (num : xs)
 evaluateRPNRecursive ((Right op) : rest) (x1 : x2 : xs) = evaluateRPNRecursive rest (dispatchOp op x2 x1 : xs)
 evaluateRPNRecursive ((Right op) : rest) _ = error "invalid expression"
 
-parse :: String -> Either Int Operator
+parse :: (Fractional a, Read a) => String -> Either a Operator
 parse "+" = Right Add
 parse "-" = Right Subtract
 parse "*" = Right Multiply
 parse "/" = Right Divide
 parse x = Left $ read x
 
-dispatchOp :: Operator -> Int -> Int -> Int
+dispatchOp :: Fractional a => Operator -> a -> a -> a
 dispatchOp Add = (+)
 dispatchOp Subtract = (-)
 dispatchOp Multiply = (*)
-dispatchOp Divide = div
+dispatchOp Divide = (/)
