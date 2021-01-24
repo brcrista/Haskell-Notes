fd926c108012b9e172434e35a4eeebede11bc3e4
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-01-23 20:58:34 -0500

    involutory, not involutive

--- libraries/quickCheck/testQuickCheck.hs
+++ libraries/quickCheck/testQuickCheck.hs
@@ -5,11 +5,11 @@ import Test.QuickCheck
 commutative :: Eq b => (a -> a -> b) -> a -> a -> Bool
 commutative f x y = f x y == f y x
 
-involutive :: Eq a => (a -> a) -> a -> Bool
-involutive f x = f (f x) == x
+involutory :: Eq a => (a -> a) -> a -> Bool
+involutory f x = f (f x) == x
 
-prop_reverseIsInvolutive :: [Int] -> Bool
-prop_reverseIsInvolutive = involutive reverse
+prop_reverseIsInvolutory :: [Int] -> Bool
+prop_reverseIsInvolutory = involutory reverse
 
 -- prop_integerAdditionIsCommutative :: Integral a => a -> a -> Bool
 prop_integerAdditionIsCommutative :: Int -> Int -> Bool
