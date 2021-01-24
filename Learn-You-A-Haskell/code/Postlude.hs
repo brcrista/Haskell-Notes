40fb6e8ba02dbf9a37e66d74afc7e3bb4347749a
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-01-24 11:52:38 -0500

    (|>) = (&)

--- Learn-You-A-Haskell/code/Postlude.hs
+++ Learn-You-A-Haskell/code/Postlude.hs
@@ -1,13 +1,9 @@
 -- This module contains Prelude-like functions that aren't actually in Prelude.
 module Postlude(
-  (|>),
   negative,
   cartesian
 )
 where
-
-(|>) :: a -> (a -> b) -> b
-(|>) = flip ($)
 
 negative :: Integer -> Integer
 negative = negate . abs
