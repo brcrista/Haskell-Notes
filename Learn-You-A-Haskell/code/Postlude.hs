c7d00713d7eaf65a4e2cea1e139c9e1136fd2dec
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-02-13 16:24:38 -0500

    W combinator = Control.Monad.join

--- Learn-You-A-Haskell/code/Postlude.hs
+++ Learn-You-A-Haskell/code/Postlude.hs
@@ -1,6 +1,5 @@
 -- This module contains Prelude-like functions that aren't actually in Prelude.
 module Postlude(
-  duplicate,
   negative,
   cartesian,
   cartesian3,
@@ -9,11 +8,6 @@ module Postlude(
 where
 
 import Control.Monad
-
--- W combinator
--- Turn a binary function into a unary function by duplicating its argument.
-duplicate :: (a -> a -> b) -> a -> b
-duplicate f x = f x x
 
 negative :: Integer -> Integer
 negative = negate . abs
@@ -25,7 +19,7 @@ cartesian3 :: Applicative f => f a -> f b -> f c -> f (a, b, c)
 cartesian3 xs ys zs = (,,) <$> xs <*> ys <*> zs
 
 cartesianPower :: Applicative f => f a -> f (a, a)
-cartesianPower = duplicate cartesian
+cartesianPower = join cartesian
 
 curry3 :: ((a, b, c) -> d) -> a -> b -> c -> d
 curry3 f x y z = f (x, y, z)
