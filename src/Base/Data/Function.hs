26693dc940e9a2bdd31a51d297a594fc7baf7e67
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-02-15 16:53:47 -0500

    add & and on

--- src/Base/Data/Function.hs
+++ src/Base/Data/Function.hs
@@ -11,11 +11,16 @@ const x _ = x
 flip :: (a -> b -> c) -> (b -> a -> c)
 flip f y x = f x y
 
-($) :: (a -> b) -> a -> b
-f $ x = f x
-
 (.) :: (b -> c) -> (a -> b) -> (a -> c)
 (f . g) x = f (g x)
 
--- TODO `on`
+($) :: (a -> b) -> a -> b
+f $ x = f x
+
+(&) :: a -> (a -> b) -> b
+x & f = f x
+
+on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
+(f `on` g) x y = f (g x) (g y)
+
 -- TODO `fix`
