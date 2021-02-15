7c1c7114c4023821e6cd16298b2fae82932121e7
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-02-15 13:16:52 -0500

    Use a `src` directory to organize modules

--- src/Postlude.hs
+++ src/Postlude.hs
@@ -1,30 +1,23 @@
 -- This module contains Prelude-like functions that aren't actually in Prelude.
-module Postlude(
-  cartesian,
-  cartesian3,
-  cartesianPower,
-  curry3,
-  first,
-  negative,
-  toList,
-  uncurry3
-)
-where
+module Postlude where
 
 import Control.Applicative (liftA2, liftA3)
 import Control.Monad (join)
 
-negative :: Integer -> Integer
+negative :: Num a => a -> a
 negative = negate . abs
 
+-- | The Cartesian product `S1 x S2`.
 cartesian :: Applicative f => f a -> f b -> f (a, b)
 cartesian = liftA2 (,)
 
+-- | The Cartesian product `S1 x S2 x S3`.
 cartesian3 :: Applicative f => f a -> f b -> f c -> f (a, b, c)
 cartesian3 = liftA3 (,,)
 
-cartesianPower :: Applicative f => f a -> f (a, a)
-cartesianPower = join cartesian
+-- | The Cartesian product of a set with itself, e.g. `R^2`, 
+cartesianSquare :: Applicative f => f a -> f (a, a)
+cartesianSquare = join cartesian
 
 curry3 :: ((a, b, c) -> d) -> a -> b -> c -> d
 curry3 f x y z = f (x, y, z)
@@ -35,10 +28,12 @@ uncurry3 f (x, y, z) = f x y z
 toList :: Foldable t => t a -> [a]
 toList = foldr (:) []
 
--- Using `zipWith` instead of length` lets this work with infinite lists.
+-- | The range of indices for a list.
+-- Implementation note: Using `zipWith` instead of length` lets this work with infinite lists.
 indices :: [a] -> [Int]
 indices = zipWith const [0 ..]
 
+-- | The first `Just` element in a list or `Nothing` if there is none.
 first :: [Maybe a] -> Maybe a
 first ((Just x) : xs) = Just x
 first (Nothing  : xs) = first xs
