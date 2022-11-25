2fbe8fe8583ecc5a57bdf399a5f1c78e09331c0f
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2022-11-24 21:14:09 -0500

    Add exercises

---
+++ Exercises/ListRemove.hs
@@ -0,0 +1,8 @@
+remove :: Int -> [a] -> [a]
+remove _ [] = []
+remove n (x:xs) = removeRecursive n x xs
+
+removeRecursive :: Int -> a -> [a] -> [a]
+removeRecursive _ _ [] = []
+removeRecursive 0 _ xs = xs
+removeRecursive n next (x:xs) = next : (removeRecursive (n - 1) x xs)
