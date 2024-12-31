59820262a852deb84c6bf68ba2c4991bc0d0bba2
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2024-12-31 14:58:40 -0500

    Update to GHC 9.8 (#12)

--- Exercises/src/ListRemove.hs
+++ Exercises/src/ListRemove.hs
@@ -9,4 +9,4 @@ remove n (x:xs) = removeRecursive n x xs
 removeRecursive :: Int -> a -> [a] -> [a]
 removeRecursive _ _ [] = []
 removeRecursive 0 _ xs = xs
-removeRecursive n next (x:xs) = next : (removeRecursive (n - 1) x xs)
+removeRecursive n next (x:xs) = next : removeRecursive (n - 1) x xs
