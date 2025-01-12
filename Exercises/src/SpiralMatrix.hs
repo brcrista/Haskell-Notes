6960ce7f0df6a183413a05cf986cb93ee02eedea
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2025-01-11 23:22:34 -0500

    Spiral matrix redux (#14)
    * toList exists in Data.Foldable
    
    * Fix build
    
    * Break out more top-level functions
    
    * reorder

--- Exercises/src/SpiralMatrix.hs
+++ Exercises/src/SpiralMatrix.hs
@@ -6,19 +6,22 @@ type Matrix a = [[a]]
 
 spiral :: Matrix a -> [a]
 spiral [] = []
-spiral matrix = outerMatrix ++ spiral innerMatrix
+spiral matrix = outerMatrix matrix ++ spiral (innerMatrix matrix)
+
+innerMatrix :: [[a]] -> [[a]]
+innerMatrix matrix = [middle row | row <- middle matrix]
+
+outerMatrix :: [[a]] -> [a]
+outerMatrix matrix = concat [topRow, middle rightColumn, reverse bottomRow, reverse $ middle leftColumn]
   where
+    height      = length matrix
     -- Assume the matrix is not jagged.
     width       = length $ head matrix
-    height      = length matrix
     topRow      = head matrix
     rightColumn = column (width - 1) matrix
     -- If we only have one row or column, make sure we don't double-count.
     bottomRow   = if height /= 1 then last     matrix else []
     leftColumn  = if width  /= 1 then column 0 matrix else []
-    -- Just take the middle elements from the columns so they're not double-counted.
-    outerMatrix = concat [topRow, middle rightColumn, reverse bottomRow, reverse $ middle leftColumn]
-    innerMatrix = [middle row | row <- middle matrix]
 
 middle :: [a] -> [a]
 middle []  = []
@@ -26,4 +29,4 @@ middle [_] = []
 middle xs  = tail . init $ xs
 
 column :: Int -> Matrix a -> [a]
-column i = map (!! i)
+column i = fmap (!! i)
