ab65be58076787b857da91ba265158bfe9d2a583
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2025-01-15 22:39:00 -0500

    breadthFirstWalk

--- Exercises/src/Tree.hs
+++ Exercises/src/Tree.hs
@@ -5,10 +5,12 @@ data Tree a = Empty | Tree a (Tree a) (Tree a)
   deriving (Eq, Show)
 
 instance Functor Tree where
+  fmap :: (a -> b) -> Tree a -> Tree b
   fmap f Empty = Empty
   fmap f (Tree x left right) = Tree (f x) (fmap f left) (fmap f right)
 
 instance Foldable Tree where
+  foldr :: (a -> b -> b) -> b -> Tree a -> b
   foldr _ acc Empty = acc
   foldr f acc (Tree x left right) = f x (foldr f (foldr f acc right) left)
 
@@ -55,3 +57,28 @@ rotateRight tree@(Tree x left right)
   = case right of
     Empty -> tree
     Tree y inner outer -> Tree y (Tree x left inner) outer
+
+-- TODO the different walk orders could probably be different instances of `Foldable`.
+-- | Visit tree nodes in breadth-first order.
+type Queue a = [a]
+
+push :: a -> Queue a -> Queue a
+push = (:)
+
+-- In base-4.19 this is `Data.List.usnoc`
+pull :: Queue a -> Maybe (Queue a, a)
+pull [] = Nothing
+pull xs = Just (init xs, last xs)
+
+breadthFirstWalk :: Show a => Tree a -> IO ()
+breadthFirstWalk tree = breadthFirstWalkRecursive [tree | not (null tree)]
+
+breadthFirstWalkRecursive :: Show a => Queue (Tree a) -> IO ()
+breadthFirstWalkRecursive [] = pure ()
+breadthFirstWalkRecursive queue = do
+  let Just (queue', node) = pull queue
+  let Tree x left right = node
+  print x
+  let queue'' = if null left then queue' else push left queue'
+  let queue''' = if null right then queue'' else push right queue''
+  breadthFirstWalkRecursive queue'''
